fe15de41687068a2898062cfaeeb186ee8a14a21
Purya Zarchiny <puryazarchiny@gmail.com>
2024-05-15 20:14:33 +0330

    build: repository init

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { es2016: true, node: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-restricted-imports": ["error", { patterns: ["@/features/*/*"] }],
+  },
+};
