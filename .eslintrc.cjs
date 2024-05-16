bf9cc2b9077c7da56c633a96ac865961d50bc614
Purya Zarchiny <puryazarchiny@gmail.com>
2024-05-16 16:02:10 +0330

    build: ignore dist directory in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { es2016: true, node: true },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
